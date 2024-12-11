808e8d79f8cc8aa1b50d5f188421b3ca5edcb49e
Matti Barlach <mattibarlach@gmail.com>
2024-12-11 12:59:48 +0100

    change back

--- DeskSensorRESTService/Program.cs
+++ DeskSensorRESTService/Program.cs
@@ -9,9 +9,9 @@ builder.Services.AddControllers();
 
 var optionsBuilder = new DbContextOptionsBuilder<DeskDbContext>();
 
-string? connectionString = Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING");
+//string? connectionString = Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING");
 
-optionsBuilder.UseSqlServer(connectionString);
+optionsBuilder.UseSqlServer("Data Source=mssql12.unoeuro.com;Initial Catalog=auden_dk_db_eksamen;User ID=auden_dk;Password=5pFwR4c9bfEDGe3Bdymh;TrustServerCertificate = True");
 DeskDbContext dbContext = new(optionsBuilder.Options);
 
 builder.Services.AddSingleton<DeskRepoDb>(new DeskRepoDb(dbContext));
